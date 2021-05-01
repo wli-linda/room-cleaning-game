7630db48aa8560e9798ce50b798609202db52f42
ziting_pis_vm <cherryquinnlg@gmail.com>
2021-05-01 04:52:03 -0400

    room rendering start

--- vroomba/RoomRendering.ml
+++ vroomba/RoomRendering.ml
@@ -53,14 +53,28 @@ let rec wait_until_q_pressed _ =
     end
 
 (*  Top-level file for starting the game  *)
-let render_games (input_path: string) (output_path : string): unit = 
+let render_games_eg (input_path: string) (output_path : string): unit = 
   open_graph " 800x600";
   (* Example room, resized and shifted to look nice *)
   let p = [(0, 0); (2, 0); (2, -3); (3, -3); (3, -5); (4, -5); (4, -6); (1, -6); (1, -2); (-2, -2); (-2, -6); (-1, -6); (-1, -7); (-3, -7); (-3, -1); (-4, -1); (-4, 19); (-16, 19); (-16, -1); (-14, -1); (-14, -3); (-24, -3); (-24, 7); (-30, 7); (-30, -3); (-28, -3); (-28, -24); (-14, -24); (-14, -13); (-10, -13); (-10, -1); (-9, -1); (-9, -7); (-6, -7); (-6, -10); (-1, -10); (-1, -9); (4, -9); (4, -8); (6, -8); (6, -5); (6, -3); (5, -3); (5, 0); (4, 0); (4, 1); (6, 1); (6, -2); (9, -2); (9, -1); (12, -1); (12, 2); (9, 2); (9, 1); (8, 1); (8, 7); (6, 7); (6, 10); (3, 10); (3, 7); (4, 7); (4, 4); (0, 4)] 
           |> polygon_of_int_pairs 
           |> resize_polygon 80.
           |> shift_polygon (-300., -100.)
-  in
-  draw_polygon p;
+
+  in draw_polygon p;
   (* TODO: Implement the rest *)
   wait_until_q_pressed ()
+
+
+let render_games_eg (input_path: string) (output_path : string): unit = 
+  open_graph " 800x600";
+  (* Example room, resized and shifted to look nice *)
+  let play r =
+    wait_until_q_pressed ()
+  in let poly_list = file_to_polygons input_path 
+  in let room_list = List.map polygon_to_room poly_list 
+  in List.iter play room_list
+
+
+  (* TODO: Implement the rest *)
+
