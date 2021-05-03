367b897e2cbceedb715968af8048ebe8dfae0036
ziting_pis_vm <cherryquinnlg@gmail.com>
2021-05-03 14:42:31 -0400

    cannot run coz CHECK OUT the interfacing functions

--- vroomba/RoomRendering.ml
+++ vroomba/RoomRendering.ml
@@ -56,18 +56,7 @@ let rec wait_until_q_pressed _ =
       wait_until_q_pressed ()
     end
 
-(*  Top-level file for starting the game  *)
-let render_games (input_path: string) (output_path : string): unit = 
-  open_graph " 800x600";
-  (* Example room, resized and shifted to look nice *)
-  let p = [(0, 0); (2, 0); (2, -3); (3, -3); (3, -5); (4, -5); (4, -6); (1, -6); (1, -2); (-2, -2); (-2, -6); (-1, -6); (-1, -7); (-3, -7); (-3, -1); (-4, -1); (-4, 19); (-16, 19); (-16, -1); (-14, -1); (-14, -3); (-24, -3); (-24, 7); (-30, 7); (-30, -3); (-28, -3); (-28, -24); (-14, -24); (-14, -13); (-10, -13); (-10, -1); (-9, -1); (-9, -7); (-6, -7); (-6, -10); (-1, -10); (-1, -9); (4, -9); (4, -8); (6, -8); (6, -5); (6, -3); (5, -3); (5, 0); (4, 0); (4, 1); (6, 1); (6, -2); (9, -2); (9, -1); (12, -1); (12, 2); (9, 2); (9, 1); (8, 1); (8, 7); (6, 7); (6, 10); (3, 10); (3, 7); (4, 7); (4, 4); (0, 4)] 
-          |> polygon_of_int_pairs 
-          |> resize_polygon 80.
-          |> shift_polygon (-300., -100.)
-
-  in draw_polygon p;
-  (* TODO: Implement the rest *)
-  wait_until_q_pressed ()
+(* Helper functions *)
 
 let fill_poly_color ?color:(color = Graphics.black) poly_list = 
   set_color color;
@@ -76,25 +65,8 @@ let fill_poly_color ?color:(color = Graphics.black) poly_list =
 let print_tuple (x,y) =
     Printf.printf "(%d, %d)\n" x y 
 
-let check_square x y arr = 
-    let corners = [|(x, y); (x, y+1); (x+1, y); (x+1, y+1)|] in 
-    let inside = ref true and 
-        i = ref 0 in 
-    while !i < 4 && !inside do
-      begin
-      try begin
-        let (x', y') = corners.(!i) in
-        print_tuple (x', y');
-        if arr.(x').(y') == Outer then inside := false end
-      with _ -> ()
-      end;
-      i := !i + 1
-    done;
-    !inside
 
-(* let load_vroomba_picture path =  *)
-
-  
+(* Top-level funciton *)
 (* let render_games_eg2 (input_path: string) (output_path : string): unit =  *)
 let render_games_eg2 (input_path: string) (output_path : string) = 
   (* open_graph " 1800x1600"; *)
@@ -133,72 +105,29 @@ let render_games_eg2 (input_path: string) (output_path : string) =
       draw_rect final_x final_y tile_width tile_width
     done
 
+  (* TODO *)
+  in let draw_clean_boundary = ()
+  in let draw_dirty_boundary = () 
+  in let draw_vroomba boundary = ()
 
-  in let draw_vroomba r lbc_board tile_width (x,y) =
+  in let display_vroomba r lbc_board tile_width (x,y) =
     set_color Graphics.green;
     fill_rect x y tile_width tile_width 
+
 
   in let play r = 
     let (lbc_board, tile_width) = draw_board r in
     draw_room r lbc_board tile_width;
+    (* TODO: Functions that need interfacing *)
+    let state = init_state r 
+    and starting_coor = (100, 100) in
+    display_vroomba;
     wait_until_q_pressed ()
 
   in let poly_list = file_to_polygons input_path 
   in let room_list = List.map polygon_to_room_2 poly_list |> list_to_array
   in Array.iter play room_list;
   room_list.(1)
-
-
-(* let render_games_eg (input_path: string) (output_path : string): unit = 
-  open_graph " 800x600";
-  (* Check whether the block with (x,y) as the left bottom corner is 
-    inside the room *)
-  (* Linda's version: inside_room in room solver  *)
-  
-
-  (* Example room, resized and shifted to look nice *)
-  let play r =
-    (* Use the index as the coordinates first *)
-    (* Draw the board *)
-    let size = Array.length r.map 
-    and scale = 50.0
-    in let board_int_pairs = [(0, 0); (0, size); (size, size); (size, 0)] in 
-
-    let board_poly = polygon_of_int_pairs board_int_pairs in 
-    let scaled_board_poly = resize_polygon scale board_poly in
-    draw_polygon ~color:(Graphics.blue) scaled_board_poly;
-
-    (* Can print a short instruction *)
-
-    (* Fill the room with Ocaml fill_poly and then draw the room boundary 
-        with draw_polygon *)
-    (* let room_poly = !(r.edges) |> polygon_of_int_pairs in  *)
-    let room_int_pairs = !(r.edges) in 
-    let room_poly = polygon_of_int_pairs room_int_pairs in
-    let scaled_room_poly = resize_polygon scale room_poly in 
-    let scaled_room_poly_abs = shift_polygon (400.0, 300.0) scaled_room_poly in
-    (* let scaled_room_int_pairs = polygon_to_int_pairs scaled_room_poly in  *)
-    let scaled_room_int_pairs_abs = polygon_to_int_pairs scaled_room_poly_abs in
-
-    print_string @@ "scaled board_poly: " ^ (polygon_to_string scaled_board_poly) ^ "\n";
-    print_string @@ "scaled room_poly_abs: " ^ (polygon_to_string scaled_room_poly_abs) ^ "\n"; 
-    fill_poly_color ~color:(Graphics.yellow) @@ list_to_array scaled_room_int_pairs_abs;
-    draw_polygon ~color:(Graphics.red) scaled_room_poly;
-    draw_point ~color:(Graphics.green) @@ coor_to_point (605, 5);
-    wait_until_q_pressed ()
-
-    (* Draw the lines separating all the lattices *)
-
-  in let poly_list = file_to_polygons input_path 
-  (* in let room_list = List.map polygon_to_room poly_list 
-  in List.iter play room_list *)
-  in let room_list = List.map polygon_to_room poly_list 
-  in List.iter play room_list
-
-let try_eg () =
-  let f = BinaryEncodings.find_file "resources/basic.txt" 
-  in render_games_eg f ""
-  (* TODO: Implement the rest *) *)
 
 let try_eg_2 () ?file:(file = "basic") =
   let f = BinaryEncodings.find_file "resources/" ^ file ^ ".txt" in 
